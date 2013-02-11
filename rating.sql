344f90bf4fcc5d882a7e3a8f64daaba4606fafbb
Zsolt Kiraly <zkiraly@gmail.com>
2013-02-10 21:34:42 -0800

    First commit.

---
+++ rating.sql
@@ -0,0 +1,81 @@
+/* Delete the tables if they already exist */
+drop table if exists Movie;
+drop table if exists Reviewer;
+drop table if exists Rating;
+
+/* Create the schema for our tables */
+create table Movie(mID int, title text, year int, director text);
+create table Reviewer(rID int, name text);
+create table Rating(rID int, mID int, stars int, ratingDate date);
+
+/* Populate the tables with our data */
+insert into Movie values(101, 'Gone with the Wind', 1939, 'Victor Fleming');
+insert into Movie values(102, 'Star Wars', 1977, 'George Lucas');
+insert into Movie values(103, 'The Sound of Music', 1965, 'Robert Wise');
+insert into Movie values(104, 'E.T.', 1982, 'Steven Spielberg');
+insert into Movie values(105, 'Titanic', 1997, 'James Cameron');
+insert into Movie values(106, 'Snow White', 1937, null);
+insert into Movie values(107, 'Avatar', 2009, 'James Cameron');
+insert into Movie values(108, 'Raiders of the Lost Ark', 1981, 'Steven Spielberg');
+
+insert into Reviewer values(201, 'Sarah Martinez');
+insert into Reviewer values(202, 'Daniel Lewis');
+insert into Reviewer values(203, 'Brittany Harris');
+insert into Reviewer values(204, 'Mike Anderson');
+insert into Reviewer values(205, 'Chris Jackson');
+insert into Reviewer values(206, 'Elizabeth Thomas');
+insert into Reviewer values(207, 'James Cameron');
+insert into Reviewer values(208, 'Ashley White');
+
+insert into Rating values(201, 101, 2, '2011-01-22');
+insert into Rating values(201, 101, 4, '2011-01-27');
+insert into Rating values(202, 106, 4, null);
+insert into Rating values(203, 103, 2, '2011-01-20');
+insert into Rating values(203, 108, 4, '2011-01-12');
+insert into Rating values(203, 108, 2, '2011-01-30');
+insert into Rating values(204, 101, 3, '2011-01-09');
+insert into Rating values(205, 103, 3, '2011-01-27');
+insert into Rating values(205, 104, 2, '2011-01-22');
+insert into Rating values(205, 108, 4, null);
+insert into Rating values(206, 107, 3, '2011-01-15');
+insert into Rating values(206, 106, 5, '2011-01-19');
+insert into Rating values(207, 107, 5, '2011-01-20');
+insert into Rating values(208, 104, 3, '2011-01-02');
+
+/* Core set excercises */
+
+/* Question 1 */
+/* Find the titles of all movies directed by Steven Spielberg. */
+select title from Movie where director='Steven Spielberg';
+
+/* Question 2 */
+/* Find all years that have a movie that received a rating of 4 or 5, and sort them in increasing order. */
+select distinct year from Movie natural join Rating where stars >= 4 order by year;
+
+/* Question 3 */
+/* Find the titles of all movies that have no ratings. */
+select title from Movie where mID not in (select mID from Movie natural join Rating);
+
+/* Question 4 */
+/* Some reviewers didn't provide a date with their rating. Find the names of all reviewers who have ratings with a NULL value for the date. */
+select name from Reviewer natural join Rating where ratingDate is null;
+
+/* Question 5 */
+/* Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and ratingDate. Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars. */
+select name, title, stars, ratingDate from Movie natural join (Reviewer natural join Rating) order by name, title, stars;
+
+/* Question 6 */
+/* For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, return the reviewer's name and the title of the movie. */
+select name, title from Reviewer, Movie, (select distinct r1.rID, r1.mID from Rating r1, Rating r2, Movie where r1.rID = r2.rID and r1.mID = r2.mID and r1.stars > r2.stars and r1.ratingDate > r2.ratingDate) t1 where Reviewer.rID = t1.rID and Movie.mID = t1.mID;
+
+/* Question 7 */
+/* For each movie that has at least one rating, find the highest number of stars that movie received. Return the movie title and number of stars. Sort by movie title. */
+select distinct title, stars from Movie, Rating where Movie.mID = Rating.mID and stars = (select max(stars) from Rating where Rating.mID = Movie.mID) order by title
+
+/* Question 8 */
+/* List movie titles and average ratings, from highest-rated to lowest-rated. If two or more movies have the same average rating, list them in alphabetical order. */
+select distinct title, (select avg(stars) from Rating where Rating.mID = Movie.mID) s from Rating, Movie where s is not null order by s desc, title
+
+/* Question 9 */
+/* Find the names of all reviewers who have contributed three or more ratings. (As an extra challenge, try writing the query without HAVING or without COUNT.) */
+select name from (select distinct R1.rID from (Rating R1 join Rating R2) join Rating R3 where R1.rID = R2.rID and R2.rID = R3.rID and ((R1.ratingDate <> R2.ratingDate and R2.ratingDate <> R3.ratingDate and R1.ratingDate <> R3.ratingDate) or (R1.mID <> R2.mID and R2.mID <> R3.mID and R1.mID <> R3.mID))) id, Reviewer where id.rID = Reviewer.rID order by name
---
+++ social.sql
@@ -0,0 +1,100 @@
+/* Delete the tables if they already exist */
+drop table if exists Highschooler;
+drop table if exists Friend;
+drop table if exists Likes;
+
+/* Create the schema for our tables */
+create table Highschooler(ID int, name text, grade int);
+create table Friend(ID1 int, ID2 int);
+create table Likes(ID1 int, ID2 int);
+
+/* Populate the tables with our data */
+insert into Highschooler values (1510, 'Jordan', 9);
+insert into Highschooler values (1689, 'Gabriel', 9);
+insert into Highschooler values (1381, 'Tiffany', 9);
+insert into Highschooler values (1709, 'Cassandra', 9);
+insert into Highschooler values (1101, 'Haley', 10);
+insert into Highschooler values (1782, 'Andrew', 10);
+insert into Highschooler values (1468, 'Kris', 10);
+insert into Highschooler values (1641, 'Brittany', 10);
+insert into Highschooler values (1247, 'Alexis', 11);
+insert into Highschooler values (1316, 'Austin', 11);
+insert into Highschooler values (1911, 'Gabriel', 11);
+insert into Highschooler values (1501, 'Jessica', 11);
+insert into Highschooler values (1304, 'Jordan', 12);
+insert into Highschooler values (1025, 'John', 12);
+insert into Highschooler values (1934, 'Kyle', 12);
+insert into Highschooler values (1661, 'Logan', 12);
+
+insert into Friend values (1510, 1381);
+insert into Friend values (1510, 1689);
+insert into Friend values (1689, 1709);
+insert into Friend values (1381, 1247);
+insert into Friend values (1709, 1247);
+insert into Friend values (1689, 1782);
+insert into Friend values (1782, 1468);
+insert into Friend values (1782, 1316);
+insert into Friend values (1782, 1304);
+insert into Friend values (1468, 1101);
+insert into Friend values (1468, 1641);
+insert into Friend values (1101, 1641);
+insert into Friend values (1247, 1911);
+insert into Friend values (1247, 1501);
+insert into Friend values (1911, 1501);
+insert into Friend values (1501, 1934);
+insert into Friend values (1316, 1934);
+insert into Friend values (1934, 1304);
+insert into Friend values (1304, 1661);
+insert into Friend values (1661, 1025);
+insert into Friend select ID2, ID1 from Friend;
+
+insert into Likes values(1689, 1709);
+insert into Likes values(1709, 1689);
+insert into Likes values(1782, 1709);
+insert into Likes values(1911, 1247);
+insert into Likes values(1247, 1468);
+insert into Likes values(1641, 1468);
+insert into Likes values(1316, 1304);
+insert into Likes values(1501, 1934);
+insert into Likes values(1934, 1501);
+insert into Likes values(1025, 1101);
+
+/* Core problem set */
+	
+/* Problem 1
+/* Find the names of all students who are friends with someone 
+/* named Gabriel. */
+select name from Highschooler, (select ID1 from Friend, Highschooler 
+where ID2 = ID and name = 'Gabriel') as G where ID = ID1;
+
+/* Problem 2
+/* For every student who likes someone 2 or more grades younger 
+/* than themselves, return that student's name and grade, and the name and 
+/* grade of the student they like. */
+select F1.name, F1.grade, F2.name, F2.grade from Likes, Highschooler as F1, 
+Highschooler as F2 where Likes.ID1 = F1.ID and Likes.ID2 = F2.ID 
+and F1.grade-1 > F2.grade;
+
+/* Problem 3
+/* For every pair of students who both like each other, return the name and 
+/* grade of both students. Include each pair only once, with the two names 
+/* in alphabetical order. */
+select H1.name, H1.grade, H2.name, H2.grade from Highschooler as H1, 
+Highschooler as H2, (select L1.ID1, L1.ID2 from Likes as L1 join Likes as L2 
+where L1.ID1 = L2.ID2 and L1.ID2 = L2.ID1) as L where H1.ID = L.ID1 and 
+H2.ID = L.ID2 and H1.name < H2.name;
+
+/* Problem 4
+/* Find names and grades of students who only have friends in the same grade. 
+/* Return the result sorted by grade, then by name within each grade. */
+select H.name, H.grade from Highschooler as H where H.ID not in (select H1.ID 
+from Highschooler as H1, Highschooler as H2, Friend as F where F.ID1 = H1.ID 
+and F.ID2 = H2.ID and H1.grade <> H2.grade) order by H.grade, H.name;
+ 
+/* Problem 5
+/* Find the name and grade of all students who are liked by more than one 
+/* other student.  */
+select H.name, H.grade from Highschooler as H where H.ID in 
+(select L1.ID2 from Likes as L1 join Likes as L2 where L1.ID2 = L2.ID2 and 
+L1.ID1 <> L2.ID1);
+	
